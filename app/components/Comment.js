e35fabf3efa7f2ee9b0d95f254abefaaccd673fb
Dasol Kim <zoocanon92@gmail.com>
2024-08-07 00:03:38 +0900

    [modify] 댓글 표시 서식 수정

--- app/components/Comment.js
+++ app/components/Comment.js
@@ -1,5 +1,6 @@
 // app/components/Comment.js
 import { format } from 'date-fns'
+import DOMPurify from 'dompurify'
 
 export default function Comment({ comment }) {
   const formatCreatedAt = (createdAt) => {
@@ -7,13 +8,18 @@ export default function Comment({ comment }) {
     return format(date, 'yyyy. M. d. HH:mm:ss')
   };
 
+  const sanitizedContent = DOMPurify.sanitize(comment.content);
+
   return (
-    <div className="comment bg-gray-100 p-3 mb-2 rounded">
-    <div className="flex items-center mb-1">
-        <h5 className="text-sm font-semibold mr-2">{comment.authorName}</h5>
-        <span className="text-xs text-gray-500">{formatCreatedAt(comment.createdAt)}</span>
-    </div>
-    <p className="text-sm">{comment.content}</p>
+    <div className="comment-container">
+      <div className="comment-header">
+        <span className="comment-author">{comment.authorName}</span>
+        <span className="comment-date">{formatCreatedAt(comment.createdAt)}</span>
+      </div>
+      <div 
+        className="comment-content ql-editor"
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }} 
+      />
     </div>
   )
 }
